926ee0cffed69ede82984af95dae58f157a0c56f
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2024-03-10 10:09:42 +0200

    Fix Socket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ import TripSidebar from "./sidebars/TripSidebar/TripSidebar";
 import { setAuth } from "./store/authSlice";
 import { updateDriver } from "./store/driversSlice";
 
-const SOCKET_SERVER_URL = 'http://localhost:3000/admin'
+const SOCKET_SERVER_URL = 'https://medicurb.onrender.com/admin'
     
 function App() {
   const {pathname} = useLocation()
@@ -67,7 +67,7 @@ function App() {
     });
 
     socket.on("drivers-positions",(data)=>{
-      console.log("Admin Position Change : ",data);
+      console.log("Admin Position Change");
       dispatch(updateDriver(data))
     })
   
